15dc42ec4dc853cc63e0d09eb3ac621333e6737c
dongyouwei <dongyouweie@126.com>
2022-12-26 17:20:15 +0800

    fix:细节

--- Example_swift/FlexSwiftDemo/CardViewVC.swift
+++ Example_swift/FlexSwiftDemo/CardViewVC.swift
@@ -11,7 +11,8 @@ import FlexLib
 
 class CardViewVC: FlexBaseVC {
 
-    let dict = ["imageUrl":"https://image3.kfangcdn.com/prod/survey/e0dcaf63f53b43ca80f539edc92b6b47.jpg-f800x600w"]
+    let dict = ["imageUrl":"https://image3.kfangcdn.com/prod/survey/e0dcaf63f53b43ca80f539edc92b6b47.jpg-f800x600w",
+                "titleStr":"AIUSD阿萨德不吧俗话把就是不断"]
     
     
     override func viewDidLoad() {
@@ -20,8 +21,8 @@ class CardViewVC: FlexBaseVC {
         // Do any additional setup after loading the view.
         self.navigationItem.title = "CardViewVC";
 
-        
-        
+//        @objc var _modal : FlexModalView!
+        class_addIvar(Self.self, "_titleLabel", sizeof, <#T##alignment: UInt8##UInt8#>, <#T##types: UnsafePointer<CChar>?##UnsafePointer<CChar>?#>)        
     }
     
 
